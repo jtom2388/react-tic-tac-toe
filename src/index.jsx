45a33bfaaa6ede298cc9b0d7ffb00ea7bfc28ae4
jordantom <jordan.tom88@gmail.com>
2020-06-16 10:50:07 -0700

    add win logic for game

--- src/index.jsx
+++ src/index.jsx
@@ -15,6 +15,7 @@ function Game() {
 
   let [ squares, setSquares ] = useState(Array(9).fill(null));
   let [ isPlayerX, setIsPlayerX ] = useState(true);
+  let winner = win(squares);
 
   function renderSquare(n) {
     return <Square 
@@ -26,6 +27,16 @@ function Game() {
         setIsPlayerX(!isPlayerX)
       }} 
     />;
+  }
+
+  function gameStatus() {
+    if(winner) {
+      return winner + " wins!";
+    } else if (isBoardFull(squares)) {
+      return "Tie game";
+    } else {
+      return (isPlayerX ? 'X' : 'O') + "'s turn";
+    }
   }
 
   return (
@@ -48,9 +59,41 @@ function Game() {
             {renderSquare(8)}
           </div>
         </div>
+        <div className='game-info'>{gameStatus()}</div>
       </div>
     </div>
   )
 }
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+function win(squares) {
+  let threeInARow = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i = 0; i < threeInARow.length; i++) {
+    let [a, b, c] = threeInARow[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+function isBoardFull(squares) {
+  for(let i = 0; i < squares.length; i++) {
+    if(squares[i] === null) {
+      return false;
+    }
+  }
+  return true;
+}
